990db7524b1fecc390861c3643bca3046e1fb6ba
commanderZY <2453419889@qq.com>
2023-03-03 20:31:28 +0800

    Update main.go
    add get ip and board ip func

--- main.go
+++ main.go
@@ -16,3 +16,44 @@ func main() {
 	go service.WsManager.Start()
 	r.Run(":3000")
 }
+
+func getIP() (string,[]byte) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		util.Log().Error("%v", err)
+		return "",nil
+	}
+	for _, value := range addrs {
+		if ipnet, ok := value.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				util.Log().Debug(ipnet.IP.String())
+				return ipnet.IP.String(), ipnet.IP
+			}
+		}
+	}
+	return "",nil
+}
+
+func boardcastIP(ip []byte)  {
+	laddr := net.UDPAddr{
+		IP:   net.IPv4(ip[0], ip[1], ip[2], ip[3]), //局域网广播地址
+		Port: 8081,
+	}
+
+	raddr := net.UDPAddr{
+		IP:   net.IPv4(255, 255, 255, 255),//写局域网下分配IP，0.0.0.0可以用来测试
+		Port: 8081,
+	}
+
+	conn, err := net.DialUDP("udp", &laddr, &raddr)
+	if err != nil {
+		util.Log().Error(err.Error())
+	}
+	defer conn.Close()
+
+	for {
+		conn.Write(ip)
+		//util.Log().Debug("send ip:%v", ip)
+		time.Sleep(5 * time.Second)
+	}
+}
